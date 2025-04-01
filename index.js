7eed31865b3250c000989a8200a42036e7ce60b9
Shashank Manohar <146476769+Shashankmanohar@users.noreply.github.com>
2025-04-01 22:21:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin : "https://dbm-vidya-mandir-backend.vercel.app/",
+        origin : "https://www.dbmvidyamandir.com/",
         credentials:true
     }
 ));
