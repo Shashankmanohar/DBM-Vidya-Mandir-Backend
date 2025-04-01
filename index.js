a7369c22d05e465cf62f114aaee66a4e0931d69d
Shashank Manohar <146476769+Shashankmanohar@users.noreply.github.com>
2025-04-01 22:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors(
     {
         origin : "https://www.dbmvidyamandir.com/",
-         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+         methods: [ "POST"],
          allowedHeaders: ["Content-Type", "Authorization"],
         credentials:true
     }
