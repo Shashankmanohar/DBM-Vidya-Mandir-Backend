c6d53e9d5d3ecf70563dd961cd56c0482444e5cc
Shashank Manohar <146476769+Shashankmanohar@users.noreply.github.com>
2025-04-01 22:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin : "https://dbm-vidya-mandir-backend.vercel.app/",
+        credentials:true
+    }
+));
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
